fb45e4df76f69f0a60d42c20ebc1e2c08b4b042c
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-12-11 09:52:40 -0700

    added department, employee role & employees functions

--- index.js
+++ index.js
@@ -82,22 +82,56 @@ inquirer
     }
   });
   };
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
 
-// simple query
-connection.query(
-  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-  function (err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
+// Dept. function
+viewDepartments = () => {
+    console.log("Displaying departments...\n");
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+
+    connection.query(sql, (err,rows) => {
+        if (err) throw err;
+        console.table(rows);
+        questionPrompt();
+    });
+};
+
+viewEmployeeRoles = () => {
+    console.log("Displaying employee roles...\n");
+    const sql = `SELECT employee_role.id, employee_role.title, department.name AS department
+    FROM employee_role
+    INNER JOIN department ON employee_role.department_id = department.id`
+    connection.query(sql, (err,rows) => {
+        if (err) throw err;
+        console.table(rows);
+        questionPrompt();
+    });
+};
+
+viewEmployees = () => {
+    console.log("Displaying employees...\n");
+    const sql = `SELECT employee.id, 
+    employee.first_name, 
+    employee.last_name,
+    employee_role.title,
+    department.name AS department,
+    employee_role.salary,
+    FROM employee LEFT JOIN role ON employee.employee_role_id = employee_role.id
+    LEFT JOIN department ON employee_role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+    connection.query(sql, (err,rows) => {
+        if (err) throw err;
+        console.table(rows);
+        questionPrompt();
+    });
+};
+
 
 // with placeholder
 connection.query(
